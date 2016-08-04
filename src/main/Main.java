d99aaa5eea19ea6b03e510ec4620a3ac26af5cc3
Grzegorz <grzegorz.markiewicz@conceptpeople.de>
2016-08-04 10:03:03 +0200

    added some numbers

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo World!");
         System.out.println("Hallo Travis!");
+
+        for(int i=0; i<20; i++){
+            System.out.println(String.valueOf(i));
+        }
     }
 
 }
